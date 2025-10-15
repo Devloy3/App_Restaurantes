1f5b0a33b3f265a2edec1f969036e5c55c3fbaff
Eloy Dominguez <eloydominguez3@gmail.com>
2025-10-15 17:32:06 +0200

    moviendo carpetas

--- Web/server.js
+++ Web/server.js
@@ -1,3 +1,13 @@
-const express = require("express")
-const app = express()
+const express = require("express");
+const app = express();
+
+const prompt = require("prompt-sync")();
+const PORT = prompt('Insertar el puerto el cual quieras tu servidor web:');
+const direccion = prompt('Insertar la direccion a la que escucha el servidor:');
+
+app.use(express.static('public'));
+
+app.listen(PORT,() => {
+    console.log(`Servidor escuchando en http://${direccion}:${PORT}`)
+})
 
