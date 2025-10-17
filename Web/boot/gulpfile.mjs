71647d72d98ef4559620957b55876f817d3efe0c
Eloy Dominguez <eloydominguez3@gmail.com>
2025-10-17 13:07:11 +0200

    Pequeños cambios en el Proyecto

--- Web/boot/gulpfile.mjs
+++
@@ -1,7 +0,0 @@
-import gulp from 'gulp';
-import open from 'open';
-
-// Task to open the browser
-gulp.task('open-app', function(){
-  return open('/home/eloy/git/restaurantes/Web/boot/pages/dashboard.html');
-});
