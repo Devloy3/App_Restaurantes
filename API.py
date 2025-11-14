d0ebb56ae71fa678449139bef858f02e50334c5e
Eloy Dominguez <eloydominguez3@gmail.com>
2025-11-14 12:13:46 +0100

    Cambio de la base de datos

--- API.py
+++ API.py
@@ -1,7 +1,7 @@
 from flask import Flask,jsonify
 from flask import request
 from flask_cors import CORS
-from db.db import NoRelacional
+
 from datetime import date
 
 class Api:
@@ -10,32 +10,32 @@ class Api:
     
     @app.route('/', methods=['GET'])
     def mostrar_nota_media():
-        datos = NoRelacional.mostar_nota_media()
+    
         return jsonify([{"Nombre": nombre, "Nota": nota } for nota,nombre in datos])
 
     @app.route('/mostrar_puntaje', methods=['GET'])
     def mostrar_todo():
-        datos = NoRelacional.mostrar_todos()
+        
         return jsonify([{"Nombre": nombre, "Decoracion": decoracion, "Menu": menu, "Cocina": cocina, "Servicio": servicio, "Precio": precio} for nombre,decoracion,menu,cocina,servicio,precio in datos])
     
     @app.route('/decoracion', methods=['GET'])
     def decoracion():
-        datos = NoRelacional.ordenar_por_decoracion()
+
         return jsonify([{"Nombre": nombre, "Decoracion": decoracion, "Menu": menu, "Cocina": cocina, "Servicio": servicio, "Precio": precio} for nombre,decoracion,menu,cocina,servicio,precio in datos])
     
     @app.route('/menu', methods=['GET'])
     def menu():
-        datos = NoRelacional.ordenar_por_menu()
+        
         return jsonify([{"Nombre": nombre, "Decoracion": decoracion, "Menu": menu, "Cocina": cocina, "Servicio": servicio, "Precio": precio} for nombre,decoracion,menu,cocina,servicio,precio in datos])
     
     @app.route('/servicio', methods=['GET'])
     def servicio():
-        datos = NoRelacional.ordenar_por_servicio()
+        
         return jsonify([{"Nombre": nombre, "Decoracion": decoracion, "Menu": menu, "Cocina": cocina, "Servicio": servicio, "Precio": precio} for nombre,decoracion,menu,cocina,servicio,precio in datos])
     
     @app.route('/precio', methods=['GET'])
     def precio():
-        datos = NoRelacional.ordenar_por_precio()
+      
         return jsonify([{"Nombre": nombre, "Decoracion": decoracion, "Menu": menu, "Cocina": cocina, "Servicio": servicio, "Precio": precio} for nombre,decoracion,menu,cocina,servicio,precio in datos])
     
     @app.route('/insertar_restaurante', methods=['POST'])
@@ -46,7 +46,7 @@ class Api:
         comida = float(request.form.get("comida"))
         servicio = float(request.form.get("servicio"))
         precio = float(request.form.get("precio"))
-        NoRelacional.crear_restaurante(nombre,decoracion,menu,comida,servicio,precio)
+        
         return jsonify ({"Nombre": nombre,
                          "Decoracion": decoracion,
                          "Menu": menu,
@@ -58,14 +58,14 @@ class Api:
     
     @app.route('/nota_fecha', methods=['GET'])
     def promedio_con_fecha():
-        nota = NoRelacional.mostar_el_promedio_total()
+
         fecha = date.today()
         fecha_string = fecha.strftime("%d-%m-%y")
         return jsonify({"Nota": nota, "Fecha": fecha_string})
     
     @app.route('/nota', methods=['GET'])
     def promedio():
-        promedio = NoRelacional.mostar_el_promedio_total()
+        
         return jsonify({"Nota": promedio})
     
     @classmethod
